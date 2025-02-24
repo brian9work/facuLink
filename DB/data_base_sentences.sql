e122aa418f18febecf4d23de3877b5387979748f
Brian <brian9work@gmail.com>
2025-02-23 23:18:01 -0600

    creation of files for a Api in a mode static

--- DB/data_base_sentences.sql
+++ DB/data_base_sentences.sql
@@ -1,7 +1,23 @@
 -- Seleccionar todas las paradas de autobús que se encuentren a una distancia de 1 km de la ubicación (19.41514082532041, -98.14024764753933) en metros.
-
 SELECT 
-    *,
+    s.id AS id_stop,
+    s.longitude,
+    s.latitude,
+    s.name AS stop_name,
+    s.imagen AS stop_imagen,
+    t.id AS id_transport,
+    t.frequency,
+    t.name AS transport_name,
+    t.imagen As transport_imagen,
+    t.origin,
+    t.destination,
+    clt.id AS id_line_of_transport,
+    clt.name AS line_of_transport_name,
+    clt.complete_name,
+    clt.imagen AS line_of_transport_imagen,
+    ctt.id AS id_type_of_transport,
+    ctt.name AS type_of_transport_name,
+    ctt.imagen AS type_of_transport_imagen,
     6371000 * 2 * ASIN(
         SQRT(
             POWER(SIN((RADIANS(-98.14024764753933) - RADIANS(CAST(latitude AS DECIMAL(18, 15)))) / 2), 2) +
@@ -13,9 +29,13 @@ SELECT
 FROM 
     stop s
 JOIN 
-    transport_stop ts ON s.id = ts.id_stop
+    transport_stop ts ON ts.id_stop = s.id
 JOIN 
     transport t ON ts.id_transport = t.id
+JOIN 
+    cat_line_of_transport clt ON clt.id = t.id_type_of_transport
+JOIN 
+    cat_type_of_transport ctt ON ctt.id = t.id_type_of_transport
 WHERE 
     -- Filtro para paradas dentro de 10 metros
     6371000 * 2 * ASIN(
@@ -25,9 +45,11 @@ WHERE
             COS(RADIANS(CAST(latitude AS DECIMAL(18, 15)))) *
             POWER(SIN((RADIANS(19.41514082532041) - RADIANS(CAST(longitude AS DECIMAL(18, 15)))) / 2), 2)
         )
-    ) <= 10000 -- distancia en metros
+    ) <= 1000 -- distancia en metros
 ORDER BY 
     distance_in_meters;
+
+
 
 
 
